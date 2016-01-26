741291857e20d968bc015f3b26eddde85a03f19e
dionnecf <dcondor_farrell@yahoo.co.uk>
2016-01-26 13:58:22 +0000

    New entity to resolve many to many relationship between User and Project.  ProjectVolunteer acts as a link table but with extra attributes that are required.  Data Access Layer (Entities and Repositories) for WHFNP Portal API

---
+++ src/main/java/com/whfnp/portal/model/ProjectVolunteer.java
@@ -0,0 +1,47 @@
+package com.whfnp.portal.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by Dionne on 25/01/2016.
+ */
+@Entity
+@IdClass(ProjectVolunteerId.class)
+public @Data class ProjectVolunteer {
+
+    @EmbeddedId
+    private ProjectVolunteerId id = new ProjectVolunteerId();
+
+    @Column(name="user_id")
+    private User user;
+
+    @Column(name="project_id")
+    private Project project;
+
+    @Column(name="role_on_project", nullable=true)
+    private long roleOnProject;
+
+    @ManyToOne(optional=true)
+    @JoinColumn(name="status", nullable=true)
+    private Status status;
+
+}
+
+class ProjectVolunteerId implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne(optional=false)
+    private User user;
+
+    @ManyToOne(optional=false)
+    private Project project;
+
+}
+
+
